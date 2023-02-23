0b6dc4e91fe5a2982d95ca48d8a3bafc4171b409
TomA!s de Campos Sucena de Sequeiros Lopes <tomas21sucena@gmail.com>
2023-02-23 16:15:12 +0000

    updated utils.c

--- lab2/utils.c
+++ lab2/utils.c
@@ -3,22 +3,19 @@
 #include <stdint.h>
 
 int(util_get_LSB)(uint16_t val, uint8_t *lsb) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
+  if (lsb == NULL) return 1;
 
-  return 1;
+  *lsb = (uint8_t) val;
+  return 0;
 }
 
 int(util_get_MSB)(uint16_t val, uint8_t *msb) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
+  if (msb == NULL) return 1;
 
-  return 1;
+  *msb = (uint8_t) val >> 8;
+  return 0;
 }
 
 int (util_sys_inb)(int port, uint8_t *value) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
-
-  return 1;
+  return sys_inb(port, (uint32_t) value);
 }
